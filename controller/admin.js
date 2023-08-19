d8d45394281754edcfd6ba496b3c7cf7e0301824
Abdulrahmon6764 <abdulrahmonu@gmail.com>
2023-08-19 02:19:38 -0700

    backend

--- controller/admin.js
+++
@@ -1,7 +0,0 @@
-// const {validationResult} = require('express-validator');
-
-// exports.homePage = (req,res)=>{
-//     let errMsg = req.flash('validationErr')
-//         console.log(errMsg)
-//         res.render('index.ejs',{title:"Home",DisplayErros:errMsg,modalOpen:true})
-// }
