44251da04ecab4bc3caeb79dfe23f9e2da9ae453
Abdulrahmon6764 <abdulrahmonu@gmail.com>
2023-08-19 02:13:17 -0700

    backend

--- controller/admin.js
+++ controller/admin.js
@@ -1,7 +1,7 @@
 // const {validationResult} = require('express-validator');
 
-exports.homePage = (req,res)=>{
-    let errMsg = req.flash('validationErr')
-        console.log(errMsg)
-        res.render('index.ejs',{title:"Home",DisplayErros:errMsg,modalOpen:true})
-}
+// exports.homePage = (req,res)=>{
+//     let errMsg = req.flash('validationErr')
+//         console.log(errMsg)
+//         res.render('index.ejs',{title:"Home",DisplayErros:errMsg,modalOpen:true})
+// }
--- routes/route.js
+++ routes/route.js
@@ -4,33 +4,33 @@ const adminController = require('../controller/admin');
 const authen = require('../controller/auth/authen');
 
 
-router.get('/',adminController.homePage);
+// router.get('/',adminController.homePage);
 
-router.post('/register-Page',[
-check('firstName').notEmpty().withMessage('Field must not be blank').ltrim().withMessage("please remove the whitespace").rtrim().withMessage('please remove whitespace'),
-check('lastName').notEmpty().withMessage('Field must not be blank').ltrim().withMessage("please remove the whitespace").rtrim().withMessage('please remove whitespace'),
-check('Email').notEmpty().withMessage('Field must not blank').isEmail().withMessage('Invalid Email').ltrim().withMessage('please remove the whitespace').rtrim().withMessage('please remove the whitespace'),
-check('Password').notEmpty().withMessage('Field cannot be blank').isLength({min:6}).withMessage('Pass word must not less than 6 character').contains('$@_-').withMessage('contain any character $@_-'),
-check('confirmPassword').notEmpty().withMessage('Field cannot be blank').custom((value,{req})=>{
-    if(value !== req.body.password){
-        throw new Error('Password not match')
-    }
-    return true
-})
+// router.post('/register-Page',[
+// check('firstName').notEmpty().withMessage('Field must not be blank').ltrim().withMessage("please remove the whitespace").rtrim().withMessage('please remove whitespace'),
+// check('lastName').notEmpty().withMessage('Field must not be blank').ltrim().withMessage("please remove the whitespace").rtrim().withMessage('please remove whitespace'),
+// check('Email').notEmpty().withMessage('Field must not blank').isEmail().withMessage('Invalid Email').ltrim().withMessage('please remove the whitespace').rtrim().withMessage('please remove the whitespace'),
+// check('Password').notEmpty().withMessage('Field cannot be blank').isLength({min:6}).withMessage('Pass word must not less than 6 character').contains('$@_-').withMessage('contain any character $@_-'),
+// check('confirmPassword').notEmpty().withMessage('Field cannot be blank').custom((value,{req})=>{
+//     if(value !== req.body.password){
+//         throw new Error('Password not match')
+//     }
+//     return true
+// })
 
-],authen.register);
+// ],authen.register);
 
 // router.get('/register-Page',authen.registerPage);
 
-router.get('/login-page',authen.loginPage);
-router.post('/login-page',[
-    check('Email').notEmpty().withMessage('Field must not blank').isEmail().withMessage('Invalid Email').ltrim().withMessage('please remove the whitespace').rtrim().withMessage('please remove the whitespace'),
-    check('Password').notEmpty().withMessage('Field cannot be blank').isLength({min:6}).withMessage('Pass word must not less than 6 character').contains('$@_-').withMessage('contain any character $@_-'),
-    check('confirmPassword').notEmpty().withMessage('Field cannot be blank').custom((value,{req})=>{
-        if(value !== req.body.password){
-            throw new Error('Password not match')
-        }
-        return true
-    })
-],authen.login);
+// router.get('/login-page',authen.loginPage);
+// router.post('/login-page',[
+//     check('Email').notEmpty().withMessage('Field must not blank').isEmail().withMessage('Invalid Email').ltrim().withMessage('please remove the whitespace').rtrim().withMessage('please remove the whitespace'),
+//     check('Password').notEmpty().withMessage('Field cannot be blank').isLength({min:6}).withMessage('Pass word must not less than 6 character').contains('$@_-').withMessage('contain any character $@_-'),
+//     check('confirmPassword').notEmpty().withMessage('Field cannot be blank').custom((value,{req})=>{
+//         if(value !== req.body.password){
+//             throw new Error('Password not match')
+//         }
+//         return true
+//     })
+// ],authen.login);
 module.exports=router;
