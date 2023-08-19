eca53e919544b3230e75c475f67e64e9a4430bba
Abdulrahmon6764 <abdulrahmonu@gmail.com>
2023-08-19 10:05:34 -0700

    routes

--- controller/auth/authen.js
+++ controller/auth/authen.js
@@ -4,3 +4,19 @@ const session = require('../../modal/sessions');
 const {validationResult} = require('express-validator');
 
 
+
+exports.register = (req,res) =>{
+
+}
+
+exports.registerPage = (req,res) =>{
+
+}
+
+exports.login = (req,res) =>{
+
+}
+
+exports.loginPage = (req,res) =>{
+
+}
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,18 @@
-// const router = require('express').Router()
-// const { check } = require('express-validator');
-// const adminController = require('../controller/admin');
-// const authen = require('../controller/auth/authen');
+const router = require('express').Router()
+const { check } = require('express-validator');
+const adminController = require('../controller/admin');
+const authen = require('../controller/auth/authen');
+
+router.get('/',adminController.homePage);
+
+router.get('/register-page',authen.registerPage);
+
+router.post('/register-page',authen.register);
+
+router.get('/login-page',authen.loginPage);
+
+router.post('/login-page',authen.login);
 
 
 
-// module.exports=router;
+module.exports=router;
