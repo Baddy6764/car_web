46276bfc087fed09104ee62179c1e2de299c36a7
Abdulrahmon6764 <lacymia679@gmail.com>
2023-08-21 02:39:34 -0700

    Save user

--- controller/auth/authen.js
+++ controller/auth/authen.js
@@ -8,44 +8,42 @@ const users = require("../../modal/users");
 exports.register = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, password, confirmPassword } = req.body;
 
-  res.send(req.body)
+  const isEmail = await Users.findOne({ email: email });
 
-  // const isEmail = await Users.findOne({ email: email });
+  if (isEmail) {
+    res.status(400);
+    throw new Error({ success: false, message: "email already exist!" });
+  }
+  if (password != confirmPassword) {
+    res.status(400);
+    throw new Error({ success: false, message: "password not matched!" });
+  }
 
-  // if (isEmail) {
-  //   res.status(400);
-  //   throw new Error({ success: false, message: "email already exist!" });
-  // }
-  // if (password != confirmPassword) {
-  //   res.status(400);
-  //   throw new Error({ success: false, message: "password not matched!" });
-  // }
+  const token = await jwt.sign(
+    { email, lastName, firstName, password },
+    "112345"
+  );
 
-  // const token = await jwt.sign(
-  //   { email, lastName, firstName, password },
-  //   "112345"
-  // );
+  sendMail({
+    email: email,
+    subject: "Gart account activation",
+    message: token,
+  });
 
-  // sendMail({
-  //   email: email,
-  //   subject: "Gart account activation",
-  //   message: token,
-  // });
+  const hashPassword = await bcrypt.hash(password, 13);
+  const user = await users.create({
+    firstname: firstName,
+    lastName: lastName,
+    email: email,
+    password: hashPassword,
+  });
 
-  // const hashPassword = await bcrypt.hash(password, 13);
-  // const user = await users.create({
-  //   firstname: firstName,
-  //   lastName: lastName,
-  //   email: email,
-  //   password: hashPassword,
-  // });
-
-  // res.send({
-  //   success: true,
-  //   message: "Verification link have been sent to your email",
-  //   data: user,
-  //   token: email,
-  // });
+  res.send({
+    success: true,
+    message: "Verification link have been sent to your email",
+    data: user,
+    token: email,
+  });
 });
 // exports.registerPage = (req,res) =>{
 
