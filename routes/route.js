2bfdc61016ccf2a256e92484f55263d2a68949aa
Abdulrahmon6764 <abdulrahmonu@gmail.com>
2023-08-22 13:20:21 -0700

    login

--- routes/route.js
+++ routes/route.js
@@ -1,18 +1,16 @@
-const router = require('express').Router()
-const { check } = require('express-validator');
-const adminController = require('../controller/admin');
-const authen = require('../controller/auth/authen');
+const router = require("express").Router();
+const { check } = require("express-validator");
+const adminController = require("../controller/admin");
+const authen = require("../controller/auth/authen");
 
 // router.get('/',adminController.homePage);
 
 // router.get('/register-page',authen.registerPage);
 
-router.post('/register-page',authen.register);
+router.post("/register-page", authen.register);
 
-router.post('/login-page',authen.activation);
+router.post("/activation", authen.activation);
 
-// router.post('/login-page',authen.login);
+router.post("/login-page", authen.login);
 
-
-
-module.exports=router;
+module.exports = router;
