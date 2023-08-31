0e45e80808c0be7964c9bafdd3a263ec790bd581
Abdulrahmon6764 <abdulrahmonu@gmail.com>
2023-08-31 05:37:27 -0700

    update

--- routes/route.js
+++ routes/route.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const { check } = require("express-validator");
 const adminController = require("../controller/admin");
 const authen = require("../controller/auth/authen");
-const isAuth = require('../middlewares/isAuth');
-const isAdmin = require('../middlewares/isAdmin');
+// const isAuth = require('../middlewares/isAuth');
+// const isAdmin = require('../middlewares/isAdmin');
 
 router.post("/register-page", authen.register);
 
