2720907bdc89adcf13f559b3e6c6abd1cfc136db
Abdulrahmon6764 <lacymia679@gmail.com>
2023-09-22 07:50:14 -0700

    add

--- controller/auth/authen.js
+++ controller/auth/authen.js
@@ -239,7 +239,7 @@ const GOOGLE_CLIENT_ID = "829754950475-dtkr2j0bf0sn1htrcmjs4arhmbo9jnfn.apps.goo
 passport.use(new GoogleStrategy({
     clientID:GOOGLE_CLIENT_ID,
     clientSecret:GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google",
+    callbackURL: "https://gart-api.onrender.com/auth/google",
     passReqToCallback   : true
   },
    async (request, accessToken, refreshToken, profile, done)=>{
