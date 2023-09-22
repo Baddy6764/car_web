f6af75f0801e14df23dbc09f9b2441815aae9175
Abdulrahmon6764 <lacymia679@gmail.com>
2023-09-22 06:55:36 -0700

    add

--- routes/route.js
+++ routes/route.js
@@ -40,7 +40,7 @@ router.get("/",adminController.HomePage);
 
 router.post('/auth/google',passport.authenticate('google',{scope:['profile','email']}),authen.googleAuth);
 
-router.post('/register/car',adminController.Registercars);
+router.get('/register/car',adminController.Registercars);
 
 
 // router.post("/nyname",(req,res)=>{
