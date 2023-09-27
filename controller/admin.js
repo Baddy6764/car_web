7caf0f6e5ed043bac0b45c24782e88b13739b469
Abdulrahmon6764 <lacymia679@gmail.com>
2023-09-27 04:33:13 -0700

    Multer

--- controller/admin.js
+++ controller/admin.js
@@ -24,6 +24,7 @@ res.status(200).json({data:dataJson})
 exports.Registercars =  (req,res)=>{
 //   const {make, model, generation,  engine ,videos ,images} = req.body
 console.log(req.body);
+res.status(200).json(req.body)
 }
 
 exports.updateCars = asyncHandler (async (req,res)=>{
