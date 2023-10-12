726de0977b8d59be9b11b268d6d57d10b3f823a3
Abdulrahmon6764 <lacymia679@gmail.com>
2023-10-12 04:03:53 -0700

    car-registration

--- controller/admin.js
+++ controller/admin.js
@@ -16,7 +16,7 @@ exports.datajson = (req, res) => {
 exports.Registercars = async (req, res) => {
   try {
     const { images, video, engine, generation, make, model } =  req.body;
-    const tokenUser = await req.header("token");
+    const tokenUser =  req.header("token");
     const decoded = jwt.verify(tokenUser, "12345");
     if (!decoded) {
       return res.status(401).json({ error: "User not found" });
