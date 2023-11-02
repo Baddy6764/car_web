bfbe38ac2778b01f31d3108e7d757ae9e1cd0f9f
Abdulrahmon6764 <lacymia679@gmail.com>
2023-11-02 16:34:04 -0700

    reg-car

--- controller/admin.js
+++ controller/admin.js
@@ -32,7 +32,7 @@ exports.Registercars =  (req, res) => {
   // let Image = []
     
     
-    res.status(200).send(req.files.video[0].originalname);
+    res.status(200).send(req.files.video[0].buffer);
 
     // const createdCars = carsDetails.create({
     //   Make: make,
