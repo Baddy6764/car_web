439eb1803dacbe586fe2ae4c3f1cb2b6cebb370b
Abdulrahmon6764 <lacymia679@gmail.com>
2023-11-03 17:56:11 -0700

    upload video

--- controller/admin.js
+++ controller/admin.js
@@ -34,9 +34,9 @@ exports.Registercars = async (req, res) => {
       if(!videoFile){
         return res.status(400).json({error:"videos and images not uploaded"});
       }
-      const uploadedVideo = await cloudinary.uploader.upload_stream({resource_type:"video"},(error,result)=>{
+      const uploadedVideo = await cloudinary.uploader.upload_stream({resource_type:req.files.video},(error,result)=>{
         if(error){
-      return    res.status(400).json({error:"Error uploading image"})
+      return res.status(400).json({error:"Error uploading image"})
         }
         res.status(200).json({resul:result});
       }).end(req.files.video[0].buffer.data)
